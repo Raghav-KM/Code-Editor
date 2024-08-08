0d0c907345e9380cb831c7e114c8b5ba5baeb810
Raghav <Raghav-KM>
2024-08-08 10:15:05 +0530

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="w-full h-96 bg-black"> Hello World</div>
+        </>
+    );
+}
+
+export default App;
