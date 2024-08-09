73a107341ce90de82308393055c8ca28d89aa83d
Raghav <Raghav-KM>
2024-08-09 17:00:36 +0530

    09-08-2024 : Backend [Working Code Exec]

---
+++ backend/src/index.ts
@@ -0,0 +1,107 @@
+import express from "express";
+import zod from "zod";
+import cors from "cors";
+import { exec } from "child_process";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+type STATUS = "Success" | "Error" | "Pending";
+
+type RESULT = {
+    file_id: string;
+    stderr: string;
+    stdout: string;
+};
+
+const PORT = 3000;
+
+const execution_status_map: Map<string, STATUS> = new Map();
+const execution_result_map: Map<string, RESULT> = new Map();
+
+const generate_uuid = (): string => {
+    return uuidv4();
+};
+
+app.post("/api/execute", (req, res) => {
+    const result = zod
+        .object({
+            file_id: zod.string(),
+            code: zod.string(),
+        })
+        .safeParse(req.body);
+
+    if (!result.success) {
+        res.status(411).json({
+            message: "ERR!",
+        });
+        return;
+    }
+
+    const code_id = uuidv4();
+    execution_status_map.set(code_id, "Pending");
+
+    exec("./cpp-program/program", (error, stdout, stderr) => {
+        if (error) {
+            execution_status_map.set(code_id, "Error");
+            execution_result_map.set(code_id, {
+                file_id: req.body.file_id,
+                stderr: stderr,
+                stdout: stdout,
+            });
+        } else {
+            execution_status_map.set(code_id, "Success");
+            execution_result_map.set(code_id, {
+                file_id: req.body.file_id,
+                stderr: stderr,
+                stdout: stdout,
+            });
+        }
+    });
+
+    res.json({
+        message: "Executing Code....",
+        code_id: code_id,
+    });
+});
+
+app.post("/api/check", (req, res) => {
+    const result = zod
+        .object({
+            code_id: zod.string(),
+        })
+        .safeParse(req.body);
+
+    if (!result.success) {
+        res.status(411).json({
+            message: "ERR!",
+        });
+        return;
+    }
+
+    const code_id = req.body.code_id;
+    const execution_status = execution_status_map.get(code_id);
+    if (execution_status) {
+        if (execution_status == "Pending") {
+            res.json({
+                status: "PENDING",
+            });
+        } else {
+            res.json({
+                status: execution_status,
+                ...execution_result_map.get(code_id),
+                code_id: code_id,
+            });
+        }
+    } else {
+        res.status(411).json({
+            message: "ERR! Invalid code id",
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
+});
--- frontend/src/components/CodeSection.tsx
+++ frontend/src/components/CodeSection.tsx
@@ -5,32 +5,73 @@ import { RunButton } from "./RunButton";
 import {
     AwaitingCodeResponseAtom,
     CodeResponseAtom,
+    CodeResponseType,
+    FilesAtom,
+    FileType,
     SelectedFileIdAtom,
 } from "../store/atoms/atoms";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BACKEND_URL = "http://localhost:3000";
 
 export const CodeSection = () => {
     const [loading, setLoading] = useRecoilState(AwaitingCodeResponseAtom);
+    const [code_id, setCodeId] = useState("");
+
+    const files = useRecoilValue(FilesAtom);
     const selectedFileId = useRecoilValue(SelectedFileIdAtom);
 
     const setCodeResponse = useSetRecoilState(CodeResponseAtom);
 
-    const onClickRun = () => {
-        setLoading(true);
-        setTimeout(
-            ({ id }: { id: string }) => {
-                setLoading(false);
-                setCodeResponse({
-                    response_id: "",
-                    file_id: id,
-                    status: "Error",
-                    stderr: "Code Error.... Code Error.... Code Error.... Code Error.... Code Error....",
-                    stdout: "Code Output.... Code Output.... Code Output.... Code Output.... Code Output....",
-                });
-            },
-            4000,
-            { id: selectedFileId }
-        );
+    const onClickRun = async () => {
+        try {
+            const selectedFile = files.filter(
+                (file: FileType) => file.id == selectedFileId
+            );
+
+            const response = await axios.post(`${BACKEND_URL}/api/execute`, {
+                file_id: selectedFileId,
+                code: selectedFile[0].code,
+            });
+
+            console.log(response.data);
+            setCodeId(response.data.code_id);
+            setLoading(true);
+        } catch (ex) {
+            alert("Code Execution Failed!");
+        } finally {
+        }
     };
+
+    useEffect(() => {
+        let polling_id: number | undefined;
+        if (loading && code_id != "") {
+            console.log("Polling status...");
+            polling_id = setInterval(async () => {
+                try {
+                    const response = await axios.post(
+                        `${BACKEND_URL}/api/check`,
+                        {
+                            code_id: code_id,
+                        }
+                    );
+                    console.log(response.data);
+                    if (response.data.status != "PENDING") {
+                        setCodeResponse(response.data as CodeResponseType);
+                        setLoading(false);
+                    }
+                } catch (ex) {
+                    console.log(ex);
+                    alert("Error!");
+                    setLoading(false);
+                }
+            }, 1500);
+        }
+        return () => {
+            if (polling_id) clearInterval(polling_id);
+        };
+    }, [loading,code_id]);
 
     return (
         <div className="w-full h-full border-4 border-black p-2 flex flex-col gap-2">
