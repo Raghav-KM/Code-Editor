50a1bee1cf743bcb848c5229bbed1b1e77a79cfe
Raghav <Raghav-KM>
2024-08-23 20:54:13 +0530

    23-08-2024 : Backend - File Upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { router as codeRouter } from "./routes/code";
-import { router as usersRouter } from "./routes/users";
+import { router as userRouter } from "./routes/user";
+import { router as storeRouter } from "./routes/store";
 
 import cors from "cors";
 
@@ -11,7 +12,8 @@ app.use(express.json());
 const PORT = 3000;
 
 app.use("/api/code", codeRouter);
-app.use("/api/user", usersRouter);
+app.use("/api/user", userRouter);
+app.use("/api/store", storeRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening to PORT ${PORT}`);
---
+++ backend/src/routes/store.ts
@@ -0,0 +1,100 @@
+import express from "express";
+import zod from "zod";
+import { authMiddleware } from "../middlewares/auth";
+import { PrismaClient } from "@prisma/client";
+
+export const router = express.Router();
+
+const prisma = new PrismaClient();
+
+router.get("/file/:id", (req, res) => {});
+
+router.get("/files", authMiddleware, async (req, res) => {
+    try {
+        const files = await prisma.code_file.findMany({
+            where: { userId: req.body.userId },
+        });
+
+        res.status(200).json({
+            data: files,
+        });
+    } catch (ex: any) {
+        res.status(500).json({
+            message: "ERR! Unable to fetch files",
+            error: ex.message,
+        });
+    }
+});
+
+router.put("/file/:id", (req, res) => {});
+
+const put_files_schema = zod.array(
+    zod.object({
+        id: zod.string(),
+        fileName: zod.string(),
+        code: zod.string(),
+        saved: zod.boolean(),
+    })
+);
+
+router.put("/files", authMiddleware, async (req, res) => {
+    const result = put_files_schema.safeParse(req.body);
+    if (!result.success) {
+        res.status(400).json({
+            message: result.error.errors,
+        });
+        return;
+    }
+
+    const files = result.data;
+
+    try {
+        const created_files = await Promise.all(
+            files.map(async (file) => {
+                if (!file.saved) {
+                    return prisma.code_file.upsert({
+                        where: { id: file.id },
+                        update: {
+                            fileName: file.fileName,
+                            code: file.code,
+                        },
+                        create: {
+                            id: file.id,
+                            fileName: file.fileName,
+                            code: file.code,
+                            userId: req.body.userId,
+                        },
+                    });
+                }
+            })
+        );
+        res.status(200).json({
+            message: "Files Uploaded Successfully",
+        });
+    } catch (ex: any) {
+        res.status(500).json({
+            message: "ERR! Upload Failed",
+        });
+        try {
+            const created_files = await Promise.all(
+                files.map(async (file) => {
+                    return prisma.code_file.create({
+                        data: {
+                            id: file.id,
+                            fileName: file.fileName,
+                            code: file.fileName,
+                            userId: req.body.userId,
+                        },
+                    });
+                })
+            );
+            res.status(200).json({
+                message: "Files Uploaded Successfully",
+            });
+        } catch (ex: any) {
+            res.status(500).json({
+                message: "ERR! Upload Failed",
+            });
+        }
+    }
+});
